977e7438cdd21b8ec402ade34498b6433151662a
parakalan <stsudharshan@gmail.com>
2018-10-03 03:05:40 +0000

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,81 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/18', function () {
+    return redirect('/');
+});
+
+Route::get('/currents', function () {
+    return redirect('/');
+});
+
+Route::get('/', function () {
+    return view('18.temp');
+});
+
+Route::get('/faq',function(){
+	return view('18.faq');
+});
+
+Route::get('/hospitality',function(){
+    return view('18.hospitality');
+});
+
+Route::get('/sponsors',function(){
+	return view('18.sponsors');
+});
+
+Route::get('/about',function(){
+	return view('18.aboutus');
+});
+
+Route::get('/lectures', function () {
+    return view('18.gl');
+});
+
+Route::get('/initiatives', function () {
+    return view('18.coming_soon');
+});
+
+Route::get('/events', function () {
+    return view('18.events');
+});
+
+Route::get('/workshops',function(){
+	return view('18.workshops');
+});
+
+Route::get('/schedule', function () {
+    return view('18.schedule');
+});
+
+Route::get('/outreach',function(){
+	return view('18.outreach');
+});
+
+
+Route::get('/contact', function(){
+	return view('18.temp');
+});
+
+Route::get('/magazines/{file}', function($file_name) {
+	$file_path = public_path('magazines/'.$file_name);
+	return response()->download($file_path);
+});
+
+
+Route::get('/tronicals', function() {
+	return view('18.tronicals');
+});
+
+
+
